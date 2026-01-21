df480fcda2ca17961df6ddb16fb8edc698800ba9
Boya Indu <boyaindu79@gmail.com>
2026-01-21 19:40:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,43 +1,32 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
+import re
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    name = request.args.get('name')
 
-    # If name not provided
-    if not name:
-        return """
-        <h2 style="color:red;">❌ Please provide your name in URL</h2>
-        <p>Example: <b>http://127.0.0.1:5000/?name=Noor</b></p>
-        """
+    matches = []
+    error = ""
 
-    upper_name = name.upper()
-    lower_name = name.lower()
-    reverse_name = name[::-1]
-    length_name = len(name)
+    if request.method == 'POST':
 
-    return f"""
-    <html>
-    <head>
-        <title>Flask Name Processor</title>
-    </head>
-    <body style="background-color:#f0f8ff; text-align:center; font-family:Arial;">
-    
-        <h1>👋 Hello {upper_name}</h1>
+        text = request.form['text']
+        pattern = request.form['pattern']
 
-        <h3>🔠 Uppercase: {upper_name}</h3>
-        <h3>🔡 Lowercase: {lower_name}</h3>
-        <h3>🔄 Reversed Name: {reverse_name}</h3>
-        <h3>📏 Name Length: {length_name} characters</h3>
+        try:
+            matches = re.findall(pattern, text)
 
-        <br>
-        <h2 style="color:green;">✅ Flask Query Parameter Project</h2>
+            if not matches:
+                error = "No matches found!"
 
-    </body>
-    </html>
-    """
+        except re.error:
+            error = "Invalid Regular Expression!"
+
+
+    return render_template('index.html', matches=matches, error=error)
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001)
+    app.run(debug=True)
